f0d8f4e254b571eba56d155a65b39c5647af8b82
kiruthika-090807 <192511349.simats@saveetha.com>
2026-06-19 09:50:43 +0530

    Add files via upload

---
+++ OS EXP9.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char shared_memory[1024];
+
+    strcpy(shared_memory, "Hello, shared memory!");
+
+    printf("Data written to shared memory: %s\n", shared_memory);
+
+    return 0;
+}
